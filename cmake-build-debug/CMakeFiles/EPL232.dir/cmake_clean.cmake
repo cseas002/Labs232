dd50f8dd684dd33dc1b180c4d6c9b033b98182e1
cseas002 <cseas002@ucy.ac.cy>
2021-09-08 18:59:05 +0300

    test 1

---
+++ cmake-build-debug/CMakeFiles/EPL232.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EPL232.dir/main.c.obj"
+  "EPL232.exe"
+  "EPL232.exe.manifest"
+  "EPL232.lib"
+  "EPL232.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EPL232.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
