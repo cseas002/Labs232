4834fcde1df36a306f236eddc07ccc55e70b2bc4
cseas002 <cseas002@ucy.ac.cy>
2021-09-15 10:42:04 +0300

    frontistirio 1 commit 2

--- Test1/testingFrontistirio.c
+++ Test1/testingFrontistirio.c
@@ -2,5 +2,9 @@
 
 int main()
 {
-    printf("%d", 1 + 3 * 2);
+    if (0.2) // output is true but it shows a warning 
+             //(implicit conversion from 'double' to '_Bool' changes value from 0.2 to true [-Wliteral-conversion])
+        printf("%d\n", 1 + 3 * 2);
+    for (int i = 0; i < 10; i++)
+        printf("%d\n", i);
 }
